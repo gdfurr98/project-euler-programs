3f7e691eccd81caa9ea604f858420458c8f852ec
gdfurr98 <91289301+gdfurr98@users.noreply.github.com>
2021-10-10 23:44:24 -0500

    Update largest_prime_factor.py

--- largest_prime_factor.py
+++ largest_prime_factor.py
@@ -1,10 +1,7 @@
 from sympy import factorint
-factorList = []
 finalNum = 0
 x = factorint(600851475143)
 for z in x:
-    factorList.append(z)
-for y in factorList:
-    if y > finalNum:
-        finalNum = y
+    if z > finalNum:
+        finalNum = z
 print(finalNum)
